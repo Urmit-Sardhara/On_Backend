40ba8db2a74d04ca69b145abe75e61ab60f6b0dd
urmit sardhara <sardharaurmit@gmail.com>
2026-07-19 18:13:46 +0530

    add new data base files

---
+++ src/note_app/N_app.js
@@ -0,0 +1,5 @@
+const express = require("express")
+const N_app =express();
+
+ N_app.use(express.json());
+module.exports = N_app;
--- src/server.js
+++ src/server.js
@@ -1,68 +1,10 @@
-const app =require("./app");
+const app = require ("./app.js")
 
-
-const note=[]; 
-
- // send the data 
-
-app.post("/note",(req, res)=>{
-    console.log(req.body);
-  
-    
-    note.push(req.body);
-    res.status(201).json({
-        message:"note is add"
-    })    
-
-})
-
-// fetch the data 
-
-app.get("/note",(req,res)=>{
-
-    res.status(200).json({
-    message :"done ",
-        note:note
-    
-    })
-})
-
-//delete the data 
-
-app.delete("/note/:index",(req,res)=>{
-
-    const index = req.params.index
-    if(index<0||index>=note.length){
-       return res.status(404).json({
-            
-            message:"not found"
-        })
-    }
-   
-    note.splice(index, 1)
-        res.status(200).json({
-        message:"done "
-        })
-    
-})
-// update the existing data
-app.patch("/note/:index",(req,res)=>{
-    const index=req.params.index
-    if(index<0|| index>=note.length){
-        return res.status(404).json({
-            message:"not found"
-        })
-    }
-    const Age = req.body.age
-    note[index].age=Age
-    
-    res.status(200).json({
-        message:"updated"
-    })
-
-})
+// app.get("/",(req ,res)=>{
+//    res.send("hello server")
+// })
 
 app.listen(3000,()=>{
-    console.log("server is runnig ");
+    console.log("server is runug ");
     
 })
